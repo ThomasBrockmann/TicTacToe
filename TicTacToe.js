44961f5f85c97b9607d17a4c77185e4074705bf6
Thomas Brockmann <T.Brockmann@von-schaewen.de>
2018-11-14 13:34:26 +0100

    Beautify

--- TicTacToe.js
+++ TicTacToe.js
@@ -7,26 +7,26 @@ function field(iID) {
     this.tic = false;
     this.cross = null;
     this.image = document.getElementById("f" + iID);
-    this.check = function(icross) {
+    this.check = function (icross) {
         this.cross = icross;
         this.tic = true;
         if (icross === true) {
-	    this.image.src = "cross.jpg";
-        }  else {
+            this.image.src = "cross.jpg";
+        } else {
             this.image.src = "circle.jpg";
         }
     }
-	this.reset = function() {
-		this.image.src = "blank.jpg";
-		this.tic = false;
-		this.cross = null;
-	}
+    this.reset = function () {
+        this.image.src = "blank.jpg";
+        this.tic = false;
+        this.cross = null;
+    }
 }
 // Initialization
 var count = 0;
-var fields=[];
+var fields = [];
 var i;
-for (i=1; i<=9; i++) {
+for (i = 1; i <= 9; i++) {
     fields.push(new field(i));
 }
 
@@ -43,15 +43,15 @@ function makeCross(ID) {
     if (fields[ID].tic === false) {
         fields[ID].check(true);
         document.getElementById("warning").innerHTML = "";
-		if (haveWon() === true){
-			return;
-		}
+        if (haveWon() === true) {
+            return;
+        }
         if (full() === true) {
             return;
         }
         makeCircle();
     } else {
-        document.getElementById("warning").innerHTML = "Das Feld ist bereits belegt. Bitte ein anderes wählen.";
+        document.getElementById("warning").innerHTML = "Das Feld ist bereits belegt. Bitte ein anderes wÃ¤hlen.";
     }
 }
 
@@ -60,15 +60,15 @@ function makeCircle() {
     var fieldNumber;
     do {
         fieldNumber = Math.floor((Math.random() * 9));
-    } 
+    }
     while (fields[fieldNumber].tic === true);
     fields[fieldNumber].check(false);
-	if (haveWon() === false) {
-		return;
-	}
+    if (haveWon() === false) {
+        return;
+    }
     full();
 }
- 
+
 // Checks, whether all possible moves are made. Returns true if game is over.
 function full() {
     count++;
@@ -77,44 +77,44 @@ function full() {
         document.getElementById("newGame").disabled = false;
         return true;
     }
-    return false; 
+    return false;
 }
 
 // checks, whether s.o. has won. Returns true if cross wins, false if circle wins, otherwise null.
 function haveWon() {
-	var result = "";
-	var matrix = [
-	[0,1,2],
-	[3,4,5],
-	[6,7,8],
-	[0,3,6],
-	[1,4,7],
-	[2,5,8],
-	[0,4,8],
-	[2,4,6]];
-	
-	for (x in matrix) {
-		if ((fields[matrix[x][0]].cross === true) & (fields[matrix[x][1]].cross === true) & (fields[matrix[x][2]].cross === true)) {
-	        document.getElementById("warning").innerHTML = "Du hast gewonnen";
+    var result = "";
+    var matrix = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]];
+
+    for (x in matrix) {
+        if ((fields[matrix[x][0]].cross === true) & (fields[matrix[x][1]].cross === true) & (fields[matrix[x][2]].cross === true)) {
+            document.getElementById("warning").innerHTML = "Du hast gewonnen";
             document.getElementById("newGame").disabled = false;
-			return true;
-		}
-		if ((fields[matrix[x][0]].cross === false) & (fields[matrix[x][1]].cross === false) & (fields[matrix[x][2]].cross === false)) {
-	        document.getElementById("warning").innerHTML = "Du hast verloren";
-			document.getElementById("newGame").disabled = false;
-			return false;
-		}
-	}
-	return null;
+            return true;
+        }
+        if ((fields[matrix[x][0]].cross === false) & (fields[matrix[x][1]].cross === false) & (fields[matrix[x][2]].cross === false)) {
+            document.getElementById("warning").innerHTML = "Du hast verloren";
+            document.getElementById("newGame").disabled = false;
+            return false;
+        }
+    }
+    return null;
 }
 
 // Initializes a new game. Invoked by button "new".
 function newGame() {
-	for (x in fields) {
-		fields[x].reset();
-	}
-	count = 0;
-	document.getElementById("newGame").disabled = true;
-	document.getElementById("start").disabled = false;
-	document.getElementById("warning").innerHTML = "";
+    for (x in fields) {
+        fields[x].reset();
+    }
+    count = 0;
+    document.getElementById("newGame").disabled = true;
+    document.getElementById("start").disabled = false;
+    document.getElementById("warning").innerHTML = "";
 }
